b8d95422f51f167f3ac3c24f823d62c7c8e0f74c
takamujp <afbmk248@oct.zaq.ne.jp>
2014-02-21 00:41:49 +0900

    CodeMirrorのタブキーの動作を実装

--- build/js/win-behat.js
+++ build/js/win-behat.js
@@ -64,8 +64,29 @@ angular.module('winbehat', ['ui.codemirror']);;angular.module('winbehat').contro
       indentWithTabs: false,
       extraKeys: {
         Tab: function (cm) {
-          var spaces = Array(cm.getOption('indentUnit') + 1).join(' ');
-          cm.replaceSelection(spaces, 'end', '+input');
+          var indent_unit = cm.getOption('indentUnit'), spaces = '', sel = cm.doc.sel, from = sel.from, to = sel.to, line_no = 0, len = 0, replaced = [], start = {
+              line: 0,
+              ch: 0
+            }, end = {
+              line: 0,
+              ch: 0
+            };
+          if (!cm.somethingSelected()) {
+            spaces = Array(indent_unit - from.ch % indent_unit + 1).join(' ');
+            cm.replaceSelection(spaces, 'end', '+input');
+          } else {
+            spaces = Array(indent_unit + 1).join(' ');
+            start.line = from.line;
+            for (line_no = from.line, len = line_no + cm.getSelection().split('\n').length - !to.ch; line_no < len; line_no++) {
+              replaced.push(spaces + cm.doc.getLine(line_no));
+              end.line = line_no;
+              end.ch = cm.doc.getLine(line_no).length;
+            }
+            cm.doc.replaceRange(replaced.join('\n'), start, end);
+            start.ch = from.ch + indent_unit;
+            end.ch = to.ch + indent_unit;
+            cm.doc.setSelection(start, end);
+          }
         }
       }
     };
