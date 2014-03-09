076c9c3fd6aa5ac248ae689010184bf83e75ba3a
takamujp <afbmk248@oct.zaq.ne.jp>
2014-03-09 18:44:35 +0900

    behatにパスが通っている場合は、内蔵しているbehatではなくパスが通っている方を使用するように変更

--- build/js/my-modules/behat.js
+++ build/js/my-modules/behat.js
@@ -1,8 +1,8 @@
 
 var path = require('path');
 var exec = exec = require('child_process').exec;
-var behat = path.resolve('.', '.\\behat\\vendor\\behat\\behat\\bin\\behat').replace(/\\/g, '\\\\');
-var exec_options = {
+var BEHAT_PATH = path.resolve('.', '.\\behat\\vendor\\behat\\behat\\bin\\behat').replace(/\\/g, '\\\\');
+var EXEC_OPTIONS = {
     encoding: 'utf8',
     maxBuffer: 20000*1024
 };
@@ -15,32 +15,40 @@ var exec_options = {
  * @param {string} features 実行するfeaatureファイルまたはディレクトリ
  * @param {function} callback コールバック
  */
-function RunBehat(project_dir, options, features, callback) {
-    if (options instanceof Array) {
-        options = options.join(' ');
-    } else if (typeof options == 'object') {
-        var tmp = '';
-        for (var key in options) {
-            tmp += ' ' + key + ' ' + options[key];
-        }
-        options = tmp;
-    } else if (typeof options !== 'string') {
-        options = '';
-    }
-    
-    // phpにパスが通っていることを確認してから、behatを実行する
-    exec('where php', exec_options, function (e) {
+function RunBehat(project_dir, options, features, callback) {    
+    exec('where php', EXEC_OPTIONS, function (e) {
         if (e) {
             callback(e, '', 'php.exe not found');
             return;
         }
         
-        project_dir = project_dir.replace(/([^\\])\\([^\\])/g, '$1\\\\$2');
-        features = features.replace(/([^\\])\\([^\\])/g, '$1\\\\$2');
-        
-        // cd [path] & php behat [options] [features]
-        exec('cd ' + '' + project_dir + ' & ' + 'php ' + behat + ' ' + options + ' ' + features, exec_options, function (err, stdout, stderr) {
-            callback(err, stdout, stderr);
+        exec('where behat', EXEC_OPTIONS, function (e) {
+            var behat = 'behat';
+            if (e) {
+                behat = 'php ' + BEHAT_PATH;
+            }
+            
+            (RunBehat = function RunBehat(project_dir, options, features, callback) {
+                if (options instanceof Array) {
+                    options = options.join(' ');
+                } else if (typeof options == 'object') {
+                    var tmp = '';
+                    for (var key in options) {
+                        tmp += ' ' + key + ' ' + options[key];
+                    }
+                    options = tmp;
+                } else if (typeof options !== 'string') {
+                    options = '';
+                }
+
+                project_dir = project_dir.replace(/([^\\])\\([^\\])/g, '$1\\\\$2');
+                features = features.replace(/([^\\])\\([^\\])/g, '$1\\\\$2');
+
+                // cd [path] & php behat [options] [features]
+                exec('cd ' + '' + project_dir + ' & ' + behat + ' ' + options + ' ' + features, EXEC_OPTIONS, function (err, stdout, stderr) {
+                    callback(err, stdout, stderr);
+                });
+            })(project_dir, options, features, callback);
         });
     });
 }
