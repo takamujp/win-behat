1b2a0b3008d0e931c714fe05b8ca6e8157ce6a77
takamujp <afbmk248@oct.zaq.ne.jp>
2014-04-04 23:08:05 +0900

    例外が発生してもクラッシュしないように、例外をハンドリング

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
+if (process._events.uncaughtException.length > 0) {
+    process._events.uncaughtException.splice(0, 1);
+}
+process.on('uncaughtException', function (e) {
+    console.group('Exception');
+    if (!!e.message) {
+        console.log(e.message);
+    }
+    if (!!e.stack) {
+        console.log(e.stack);
+    }
+    console.log(e);
+    console.groupEnd();
+});
+if (process._events.uncaughtException.length > 1 && !!process._events.uncaughtException[0].toString().match(/native code/)) {
+    process._events.uncaughtException.splice(0, 1);
+}
+
 angular.module('winbehat', ['ui.codemirror', 'ui.bootstrap']);
